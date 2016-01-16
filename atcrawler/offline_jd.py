e6a5158d76c0f4a7a1b1575195bd8354dba49cea
Epix <git@exz.me>
2016-01-17 00:02:45 +0800

    offline processor

---
+++ atcrawler/offline_jd.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+# Author: Epix
+import codecs
+import json
+import os
+
+from lxml import etree
+
+from atcrawler.info import *
+
+dir_path = JD_OFFLINE_FOLDER
+files = os.listdir(dir_path)
+output_file = codecs.open('pic_list.txt', 'w', 'utf8')
+for f in files:
+    j = json.load(file(os.path.join(dir_path, f), 'rb'))
+    posts = j.get('posts')
+    for post in posts:
+        content = post.get('content', '')
+        tree = etree.HTML(content)
+        imgs = tree.xpath('//img/@src')
+        for img in imgs:
+            try:
+                output_file.write(unicode(img) + '\n')
+            except:
+                pass
